0e8d3546eab8bc3e181bd787e611674c5d2a7e9e
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2024-01-22 14:08:45 +0700

    first commit

---
+++ Twitter.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract Twitter {
+    // define our struct
+    uint16 public MAX_TWEET_LENGTH = 280;
+    struct Tweet {
+        uint256 id;
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
+
+    mapping(address => Tweet[]) public tweets;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "YOU ARE NOT THE OWNER!");
+        _;
+    }
+
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner {
+        MAX_TWEET_LENGTH = newTweetLength;
+    }
+
+    function createTweet(string memory _tweet) public {
+        require(
+            bytes(_tweet).length <= MAX_TWEET_LENGTH,
+            "Tweet is too long bro!"
+        );
+        Tweet memory newTweet = Tweet({
+            id: tweets[msg.sender].length,
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function likeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id, "TWEET DOES NOT EXIST");
+        tweets[author][id].likes++;
+    }
+
+    function unlikeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id, "TWEET DOES NOT EXIST");
+        require(tweets[author][id].likes > 0, "THERE ARE NO LIKES");
+        tweets[author][id].likes--;
+    }
+
+    function getTweet(uint256 _i) public view returns (Tweet memory) {
+        return tweets[msg.sender][_i];
+    }
+
+    function getAllTweets(address _owner) public view returns (Tweet[] memory) {
+        return tweets[_owner];
+    }
+}
